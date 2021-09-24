92c5fb72f14e6cb8d671f0188e9fba602a73cebe
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-09-24 09:54:22 +0530

    update

--- Banking/src/logicalLayer/LogicalHandler.java
+++ Banking/src/logicalLayer/LogicalHandler.java
@@ -10,31 +10,32 @@ import java.util.Map;
 
 public enum LogicalHandler {
     INSTANCE;
+	PersistenceDAO persistence=new PersistenceDAOImpl();
+	InMemoryStorageDAO inMemoryStorageDAO=new InMemoryStorageDAOImpl();
 
      LogicalHandler(){
         //initially store customer table and account table in hashmap
         try {
+        	
             //get all customers and store in customer HashMap
-            List<Customer> customers = Controller.getPersistenceDAOHandler().getAllCustomers();
-            Controller.getInMemoryStorageDAOHandler().storeCustomersInCustomerMap(customers);
+            List<Customer> customers = persistence.getAllCustomers();
+            inMemoryStorageDAO.storeCustomersInCustomerMap(customers);
 
             //get all Accounts and store in Hashmap account HashMap
-            List<Account> accounts = Controller.getPersistenceDAOHandler().getAllAccounts();
-            Controller.getInMemoryStorageDAOHandler().storeAccountsInAccountMap(accounts);
+            List<Account> accounts = persistence.getAllAccounts();
+            inMemoryStorageDAO.storeAccountsInAccountMap(accounts);
         } catch (PersistenceException  e) {
             e.printStackTrace();
             System.out.println("ERROR CODE:"+e.getErrorCode() + " " + e.getMessage()+" "+e.getCause());
         }
-        catch (LogicalException e)
-        {
-            e.printStackTrace();
-            System.out.println("ERROR CODE:"+e.getErrorCode() + " " + e.getMessage()+" "+e.getCause());
-        }
-
+		/*
+		 * catch (LogicalException e) { e.printStackTrace();
+		 * System.out.println("ERROR CODE:"+e.getErrorCode() + " " +
+		 * e.getMessage()+" "+e.getCause()); }
+		 */
     }
     public void handleNewCustomer(List<Customer> customers, List<Account> accounts){
-    	PersistenceDAO persistence=new PersistenceDAOImpl();
-    	InMemoryStorageDAO inMemoryStorageDAO=new InMemoryStorageDAOImpl();
+    
           if (customers.isEmpty()|| accounts.isEmpty())
           {
             System.out.println("Handle new customers:"+"customers or accounts list is empty");
@@ -73,24 +74,25 @@ public enum LogicalHandler {
         }
         try {
             //add new account for existing customer
-             long account_id=Controller.getPersistenceDAOHandler().addAccount(customer_id, balance);
-
-             Controller.getPersistenceDAOHandler().activateCustomer(customer_id);
+             long account_id=persistence.addAccount(customer_id, balance);
+            
+             persistence.activateCustomer(customer_id);
 
              Account account=getAccountObject(customer_id,account_id,balance);
 
             //store in account hashMap
-             Controller.getInMemoryStorageDAOHandler().storeAccountInAccountMap(account);
+             inMemoryStorageDAO.storeAccountInAccountMap(account);
         }
+
         catch (PersistenceException e) {
             e.printStackTrace();
             System.out.println("ERROR CODE:"+e.getErrorCode() + " " + e.getMessage()+" "+e.getCause());
         }
-        catch (LogicalException e)
-        {
-            e.printStackTrace();
-            System.out.println("ERROR CODE:"+e.getErrorCode() + " " + e.getMessage()+" "+e.getCause());
-        }
+		/*
+		 * catch (LogicalException e) { e.printStackTrace();
+		 * System.out.println("ERROR CODE:"+e.getErrorCode() + " " +
+		 * e.getMessage()+" "+e.getCause()); }
+		 */
 
     }
     public Customer getCustomerObject(String name,int age,long phone)
@@ -132,12 +134,12 @@ public enum LogicalHandler {
     }
 
     public Map<Long, Account> getAccountsInfo(long customer_id) throws LogicalException {
-        Map<Long,Map<Long,Account>> accountMap=Controller.getInMemoryStorageDAOHandler().getAccountsMap();
+        Map<Long,Map<Long,Account>> accountMap=inMemoryStorageDAO.getAccountsMap();
         Map<Long,Account>  accountInfoMap=accountMap.get(customer_id);
-        if (accountInfoMap==null||accountInfoMap.isEmpty())
-        {
-            throw  new LogicalException("Account not found for given customer id",100);
-        }
+	
+		 if (accountInfoMap==null||accountInfoMap.isEmpty()) { 
+			 throw new LogicalException("Account not found for given customer id",100); }
+		 
         return accountInfoMap;
     }
 
@@ -146,36 +148,84 @@ public enum LogicalHandler {
         {
             System.out.println("Delete account:"+"account id or customer id is zero");
         }
-
-        Controller.getPersistenceDAOHandler().removeAccount(accountId);
+        //System.out.println("after remove");
+        persistence.removeAccount(accountId);
+        //System.out.println("after remove");
         Map<Long,Account> accountMap=getAccountsInfo(customerId);
+        //System.out.println("Before if");
         if(accountMap.size()==1)
         {
-            Controller.getPersistenceDAOHandler().deactivateCustomer(customerId);
+        	//System.out.println("inside for");
+            persistence.deactivateCustomer(customerId);
         }
+       // System.out.println("Before for");
        for (Map.Entry<Long,Account> entry: accountMap.entrySet())
        {
+    	   //System.out.println("Entered for loop");
 
            if(entry.getKey()==accountId)
            {
+        	   //System.out.println("Entered for if");
                accountMap.remove(entry.getKey(),entry.getValue());
                break;
            }
        }
     }
-    public void transactionOfWithdrawORDeposit(long customer_id,long account_id,double balance) throws LogicalException, PersistenceException{
-       Controller.getPersistenceDAOHandler().updateAccount(account_id,balance);
-        Map<Long,Account> accountHashMap=getAccountsInfo(customer_id);
-        for (Map.Entry<Long,Account> entry: accountHashMap.entrySet())
+    public void deleteCustomer(long customerId) throws PersistenceException {
+        if (customerId<=0)
         {
-            if(entry.getKey()==account_id)
-            {
-               entry.getValue().setBalance(balance);
-               break;
-            }
+            System.out.println("Delete customer:"+ "customer id is zero or less than" );
         }
+        //System.out.println("after remove");
+        persistence.deactivateCustomer(customerId);
+        persistence.deactivateAccount(customerId);
+        //System.out.println("after remove");
+        inMemoryStorageDAO.getAccountsMap().remove(customerId);
+    }
+    public String transactionOfWithdraw(long customer_id,long account_id,double balance) throws LogicalException, PersistenceException{
+    	System.out.println("submit withdraw***before get account map");
+      Map<Long,Account> accountHashMap=getAccountsInfo(customer_id);
+      System.out.println("submit withdraw***after get account map");
+      System.out.println("submit withdraw***"+accountHashMap);
+		/*
+		 * if(accountHashMap==null) { return
+		 * "given account id not available for given customer id"; }
+		 */
+      Account account=accountHashMap.get(account_id);
+      if(account.getBalance()<=balance)
+      {
+    	  return "Insufficient balance";
+      }   
+       persistence.updateAccount(account_id,(account.getBalance()-balance)); 
+       account.setBalance(account.getBalance()-balance);
+		/*
+		 * for (Map.Entry<Long,Account> entry: accountHashMap.entrySet()) {
+		 * if(entry.getKey()==account_id) {
+		 * entry.getValue().setBalance((entry.getValue().getBalance())-balance);
+		 * //return "withdraw successfully"; } }
+		 */
+        return " withdraw successsfully";
 
     }
+    public String transactionOfDeposit(long customer_id,long account_id,double balance) throws LogicalException, PersistenceException{
+        Map<Long,Account> accountHashMap=getAccountsInfo(customer_id);
+       
+		/*
+		 * Account account=accountHashMap.get(account_id);
+		 * if(account.getBalance()<=balance) { return "Insufficient balance"; }
+		 */ 
+        Account account=accountHashMap.get(account_id);
+         persistence.updateAccount(account_id,(account.getBalance()+balance)); 
+         account.setBalance(account.getBalance()+balance);
+			/*
+			 * for (Map.Entry<Long,Account> entry: accountHashMap.entrySet()) {
+			 * if(entry.getKey()==account_id) {
+			 * entry.getValue().setBalance(entry.getValue().getBalance()+balance); //return
+			 * "deposit successfully"; } }
+			 */
+          return "deposit successfully";
+
+      }
     public void closeConnection() throws LogicalException {
         Controller.getPersistenceDAOHandler().cleanUp();
     }
